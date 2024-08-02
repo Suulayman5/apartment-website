1bc059cd0dab9bcce12076f82ff53da72584208e
ajah suleiman <ajahsuleiman@gmail.com>
2024-08-02 13:22:53 +0100

    added datas for rental

---
+++ src/app/utils/Rental.js
@@ -0,0 +1,34 @@
+export default Rentals =[
+    {
+    id: 1,
+    pics: '/images/elevate-chicago-il-building-photo.jpg',
+    name: 'Residential at Banana island',
+    address: 'Banana island, Lagos',
+    price: '4 Beds: N26,000,000 - N34,000,000'
+    },
+
+    {
+    id: 2,
+    pics: '/images/mondial-river-west-chicago-il-building-photo.jpg', 
+    name: 'Condo at meitama',
+    address: 'Renko Estate Maitama, Abuja',
+    price: '2 Beds: N13,000,000 - N17,760,000'
+    },
+
+    {
+    id: 3,
+    pics: '/images/presidential-towers-chicago-il-building-photo.jpg',
+    name: 'Duplex at Lupe',
+    address: 'Oworo Estate Lupe, Abuja',
+    price: '3 Beds with Swimming Pool: N20,000,000 - N24,000,000'
+    },
+
+    {
+    id: 4,
+    pics: '/images/shoreham-and-tides-chicago-il-building-photo.jpg',
+    name: 'Duplex at Banana island',
+    address: 'Banana island, Lagos',
+    price: '3 Beds: N22,000,000 - N30,000,000'
+    },
+
+]
